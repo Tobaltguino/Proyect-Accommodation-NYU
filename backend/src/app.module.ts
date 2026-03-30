d1f4c0c9a2ef44d6c44ec41bcaa70cf7305a82b8
Jeany-B <jeany.aravena.l@hotmail.com>
2026-03-30 17:33:18 -0300

    FEATURE: Cambios para conectar a la base de datos de SupaBase

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,46 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+
 
 @Module({
-  imports: [],
+  imports: [
+
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+
+        // ESTO ES VITAL PARA SUPABASE
+        ssl: {
+          rejectUnauthorized: false,
+        },
+
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+
+
+
+
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
