ddacb88468848dc9fd48cbc57270c0752f16d8f4
Jeany-B <jeany.aravena.l@hotmail.com>
2026-06-02 19:23:06 -0400

    FEATURE: Se completa la asignación de una solicitud de un estudiante, incluyendo su entidad, servicio y controlador. Se probó pero podrían haber errores.

--- backend/src/modules/asignaciones/asignaciones.controller.ts
+++ backend/src/modules/asignaciones/asignaciones.controller.ts
@@ -1,12 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { AsignacionesService } from './asignaciones.service';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { Role } from '../auth/enums/role.enum';
+import type { AuthenticatedRequest } from 'src/common/types/authenticated-request.type';
+import { UnauthorizedException } from '@nestjs/common';
 
 @Controller('asignaciones')
 export class AsignacionesController {
-  constructor(private readonly asignacionesService: AsignacionesService) {}
+  constructor(private readonly asignacionesService: AsignacionesService) { }
 
-  @Get('status')
-  status() {
-    return this.asignacionesService.getStatus();
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN) // Solo el admin ejecuta esta acción
+  @Post()
+  crearAsignacion(
+    @Body('idSolicitud') idSolicitud: number,
+    @Body('idHabitacion') idHabitacion: number,
+    @Req() request: AuthenticatedRequest
+  ) {
+    // Extraemos el RUT del administrador directamente del Token JWT
+    const rutAdmin = request.user?.rut;
+
+    if (!rutAdmin) {
+      throw new UnauthorizedException('No se pudo obtener el RUT del administrador');
+    }
+
+    return this.asignacionesService.crearAsignacion(idSolicitud, idHabitacion, rutAdmin);
   }
 }
