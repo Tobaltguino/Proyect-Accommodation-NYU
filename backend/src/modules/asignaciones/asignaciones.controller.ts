2c10ea20842fd8284244c0da6e524456df513b77
Jeany-B <jeany.aravena.l@hotmail.com>
2026-07-04 21:27:42 -0400

    FIX: Se arregló las URL de asignaciones como recurso

--- backend/src/modules/asignaciones/asignaciones.controller.ts
+++ backend/src/modules/asignaciones/asignaciones.controller.ts
@@ -9,7 +9,8 @@ import {
   ParseIntPipe,
   Patch,
   UnauthorizedException,
-  BadRequestException
+  BadRequestException,
+  Delete
 } from '@nestjs/common';
 import { AsignacionesService } from './asignaciones.service';
 import { RolesGuard } from 'src/common/guards/roles.guard';
@@ -21,7 +22,7 @@ import { CrearAsignacionDTO } from './dto/crearAsignacion.dto';
 
 @Controller('asignaciones')
 export class AsignacionesController {
-  constructor(private readonly asignacionesService: AsignacionesService) {}
+  constructor(private readonly asignacionesService: AsignacionesService) { }
 
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.ADMIN) // Solo el admin ejecuta esta acción
@@ -45,10 +46,10 @@ export class AsignacionesController {
     );
   }
 
-  // GET http://localhost:3000/asignaciones/mi-historial
+  // GET http://localhost:3000/asignaciones/historial
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.STUDENT) // Protegido para que el estudiante acceda a su propia información
-  @Get('mi-historial')
+  @Get('historial')
   obtenerMiHistorial(@Req() request: AuthenticatedRequest) {
     // Extraemos de forma segura el RUT desde el token JWT cargado por el guardia
     const rutEstudiante = request.user?.rut;
@@ -70,10 +71,10 @@ export class AsignacionesController {
     return this.asignacionesService.obtenerTodas();
   }
 
-  // GET http://localhost:3000/asignaciones/mi-asignacion
+  // GET http://localhost:3000/asignaciones/activa
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.STUDENT) // Solo el estudiante puede preguntar por SU asignación
-  @Get('mi-asignacion')
+  @Get('activa')
   obtenerMiAsignacion(@Req() request: AuthenticatedRequest) {
     // El guardia ya verificó el token, así que extraemos el RUT con total confianza
     const rutEstudiante = request.user?.rut;
@@ -87,35 +88,36 @@ export class AsignacionesController {
     return this.asignacionesService.obtenerMiAsignacion(rutEstudiante);
   }
 
+  // GET http://localhost:3000/asignaciones/estudiantes/:rut/activa
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.ADMIN)
-  @Get('estudiante/:rut/activa')
+  @Get('estudiantes/:rut/activa')
   obtenerAsignacionActivaPorRut(@Param('rut') rut: string) {
     return this.asignacionesService.obtenerAsignacionActivaPorRut(rut);
   }
 
-  // GET http://localhost:3000/asignaciones/periodo/1
+  // GET http://localhost:3000/asignaciones/periodos/:idPeriodo
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.ADMIN)
-  @Get('periodo/:idPeriodo')
+  @Get('periodos/:idPeriodo')
   obtenerPorPeriodo(@Param('idPeriodo', ParseIntPipe) idPeriodo: number) {
     return this.asignacionesService.obtenerPorPeriodo(idPeriodo);
   }
 
-  // GET http://localhost:3000/asignaciones/residentes-activos/1
+  // GET http://localhost:3000/asignaciones/periodos/:idPeriodo/residentes/activos
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.ADMIN)
-  @Get('residentes-activos/:idPeriodo')
+  @Get('periodos/:idPeriodo/residentes/activos')
   obtenerTotalResidentesActivos(
     @Param('idPeriodo', ParseIntPipe) idPeriodo: number,
   ) {
     return this.asignacionesService.obtenerTotalResidentesActivos(idPeriodo);
   }
 
-  // PATCH http://localhost:3000/asignaciones/1/reasignar
+  // PATCH http://localhost:3000/asignaciones/:idAsignacion/habitacion
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.ADMIN)
-  @Patch(':idAsignacion/reasignar')
+  @Patch(':idAsignacion/habitacion')
   reasignarHabitacion(
     @Param('idAsignacion', ParseIntPipe) idAsignacion: number,
     @Body('idNuevaHabitacion', ParseIntPipe) idNuevaHabitacion: number,
@@ -142,10 +144,10 @@ export class AsignacionesController {
     );
   }
 
-  // PATCH http://localhost:3000/asignaciones/1/renunciar
+  // PATCH http://localhost:3000/asignaciones/:idAsignacion
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.ADMIN)
-  @Patch(':idAsignacion/renunciar')
+  @Delete(':idAsignacion') //comprobar
   renunciarAsignacion(
     @Param('idAsignacion', ParseIntPipe) idAsignacion: number,
     @Req() request: AuthenticatedRequest,
@@ -166,10 +168,10 @@ export class AsignacionesController {
   // NUEVAS RUTAS: BÚSQUEDA Y CONTROL DE ESTANCIA
   // ==========================================
 
-  // GET http://localhost:3000/asignaciones/buscar-residente/12345678-9
+  // GET http://localhost:3000/asignaciones/residentes/:rut
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.ADMIN)
-  @Get('buscar-residente/:rut')
+  @Get('residentes/:rut')
   buscarResidentePorRut(@Param('rut') rut: string) {
     if (!rut) {
       throw new BadRequestException('Debes proporcionar el RUT del estudiante.');
@@ -177,31 +179,31 @@ export class AsignacionesController {
     return this.asignacionesService.obtenerMiAsignacion(rut);
   }
 
-  // PATCH http://localhost:3000/asignaciones/1/check-in
+  // PATCH http://localhost:3000/asignaciones/:idAsignacion/ingresos
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.ADMIN)
-  @Patch(':idAsignacion/check-in')
+  @Patch(':idAsignacion/ingresos')
   registrarCheckIn(
     @Param('idAsignacion', ParseIntPipe) idAsignacion: number,
     @Req() request: AuthenticatedRequest
   ) {
     const rutAdmin = request.user?.rut;
     if (!rutAdmin) throw new UnauthorizedException('No se pudo obtener el RUT del administrador');
-    
+
     return this.asignacionesService.registrarCheckIn(idAsignacion, rutAdmin);
   }
 
-  // PATCH http://localhost:3000/asignaciones/1/check-out
+  // PATCH http://localhost:3000/asignaciones/:idAsignacion/salidas
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.ADMIN)
-  @Patch(':idAsignacion/check-out')
+  @Patch(':idAsignacion/salidas')
   registrarCheckOut(
     @Param('idAsignacion', ParseIntPipe) idAsignacion: number,
     @Req() request: AuthenticatedRequest
   ) {
     const rutAdmin = request.user?.rut;
     if (!rutAdmin) throw new UnauthorizedException('No se pudo obtener el RUT del administrador');
-    
+
     return this.asignacionesService.registrarCheckOut(idAsignacion, rutAdmin);
   }
 }
